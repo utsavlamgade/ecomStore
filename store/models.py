e56c21d500baea23c2ac495dfd65d3f1a9953321
utsavlamgade <54902689+utsavlamgade@users.noreply.github.com>
2020-07-11 20:10:31 -0400

    Product Image Field

--- store/models.py
+++ store/models.py
@@ -18,6 +18,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image._url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
